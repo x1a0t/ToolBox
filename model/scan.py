bbf7ea0c3324ec49d5db5a3a8af8180ddd9e9ba8
x1a0t <ch4ffee@gmail.com>
2020-10-12 23:42:35 +0800

    update scan.py

--- model/scan.py
+++ model/scan.py
@@ -1,4 +1,4 @@
-from requests.exceptions import SSLError, ConnectTimeout, ConnectionError
+from requests.exceptions import SSLError, ConnectTimeout, ConnectionError, ReadTimeout
 from urllib3 import disable_warnings
 
 import socket
@@ -11,11 +11,26 @@ disable_warnings()
 
 
 class Scan(object):
+    ports = []
+
     def __init__(self, **kwargs):
         self.targets = kwargs.get('targets', [])
         self.threads = kwargs.get('threads', 40)
         self.lock = threading.BoundedSemaphore(self.threads)
         self.results = []
+
+    def run(self):
+        th_pool = []
+        for ip in self.targets:
+            for port in self.ports:
+                th = threading.Thread(target=self._scan, args=(ip, port,))
+                th.start()
+                th_pool.append(th)
+        for th in th_pool:
+            th.join()
+
+    def _scan(self, ip, port):
+        pass
 
     def print(self):
         for i in self.results:
@@ -24,8 +39,29 @@ class Scan(object):
             print()
 
 
+class PortScan(Scan):
+    def __init__(self, **kwargs):
+        super(PortScan, self).__init__(**kwargs)
+
+        self.ports = kwargs.get('ports')
+
+    def _scan(self, ip, port):
+        self.lock.acquire()
+        try:
+            s = socket.socket()
+            s.settimeout(2)
+            s.connect((ip, int(port)))
+
+            self.results.append({'ip': ip, 'port': port, 'status': 'open'})
+        except socket.timeout:
+            # print('[ERR]', e)
+            self.results.append({'ip': ip, 'port': port, 'status': 'close'})
+            pass
+        self.lock.release()
+
+
 class DBScan(Scan):
-    db_ports = {
+    ports = {
         21: 'ftp', 22: 'ssh', 23: 'telnet', 389: 'ldap', 873: 'rsync',
         1433: 'mssql', 1521: 'oracle', 2181: 'zookeeper', 3690: 'svn',
         3306: 'mysql', 5000: 'DB2', 5432: 'PostGreSQL', 5984: 'CouchDB',
@@ -33,30 +69,14 @@ class DBScan(Scan):
         11211: 'memcached', 50070: 'hadoop'
     }
 
-    def run(self):
-        th_pool = []
-        for ip in self.targets:
-            for port in self.db_ports.keys():
-                th = threading.Thread(target=self._scan, args=(ip, port,))
-                th.start()
-                th_pool.append(th)
-        for th in th_pool:
-            th.join()
-
-    def print(self):
-        for i in self.results:
-            for key, value in i.items():
-                print('[+]{}: {}'.format(key, value))
-            print()
-
-    def _scan(self, ip, port: int):
+    def _scan(self, ip, port):
         self.lock.acquire()
         try:
             s = socket.socket()
             s.settimeout(2)
-            s.connect((ip, port))
+            s.connect((ip, int(port)))
 
-            self.results.append({'ip': ip, 'port': port, 'info': self.db_ports[port]})
+            self.results.append({'ip': ip, 'port': port, 'info': self.ports[port]})
         except socket.timeout:
             # print('[ERR]', e)
             pass
@@ -64,27 +84,11 @@ class DBScan(Scan):
 
 
 class WebScan(Scan):
-    web_ports = [
+    ports = [
         80, 83, 89, 8001, 8008, 8009, 8440, 8441, 8442, 8443, 8444,
         8445, 8446, 8447, 8448, 8449, 8450, 8080, 8081, 8082, 8083,
         8084, 8085, 8086, 8087, 8088, 8089, 9091
     ]
-
-    def run(self):
-        th_pool = []
-        for host in self.targets:
-            for port in self.web_ports:
-                th = threading.Thread(target=self._scan, args=(host, port,))
-                th.start()
-                th_pool.append(th)
-        for th in th_pool:
-            th.join()
-
-    def print(self):
-        for i in self.results:
-            for key, value in i.items():
-                print('[+]{}: {}'.format(key, value))
-            print()
 
     def _scan(self, host, port):
         self.lock.acquire()
@@ -100,7 +104,7 @@ class WebScan(Scan):
                     title = ''
 
                 self.results.append({'url': url, 'info': title})
-            except (SSLError, ConnectTimeout, ConnectionError):
+            except (SSLError, ConnectTimeout, ConnectionError, ReadTimeout):
                 # print('[ERR]', e)
                 pass
         self.lock.release()

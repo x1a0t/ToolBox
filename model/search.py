eb03bfa80f9b777303fa27d0c8cc94d648e520c9
x1a0t <ch4ffee@gmail.com>
2020-10-15 21:21:59 +0800

    add browser search

--- model/search.py
+++ model/search.py
@@ -1,5 +1,8 @@
 from base64 import b64encode
 from ipaddress import ip_network
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.keys import Keys
 
 import requests
 import time
@@ -7,9 +10,9 @@ import sys
 
 
 class FoFa(object):
-    hack_dict = {
+    dorks = {
         'Files Leak': 'body="index of/"',
-        'PHPinfo': 'body="<title>phpinfo()"',
+        'PHPinfo': 'body="<title>phpinfo()</title>"',
 
         'MySQL Brute': 'port=3306 && protocol=="mysql" && banner!="not allowed"',
         'MSSQL Brute': 'port=1433 && banner="MSSQL Server"',
@@ -60,9 +63,7 @@ class FoFa(object):
 
     def hack(self, target, clean=False):
         results = {}
-        for name in self.hack_dict.keys():
-            keywords = self.hack_dict[name]
-
+        for name, keywords in self.dorks.items():
             try:
                 ip_network(target, strict=False)
                 keywords += ' && ip="{}"'.format(target)
@@ -84,3 +85,61 @@ class FoFa(object):
                 continue
         return results
 
+
+class Browser(object):
+    def __init__(self, **kwargs):
+        self.options = kwargs.get('options', [''])
+        self._init_options()
+
+    def _init_options(self):
+        options = webdriver.FirefoxOptions()
+        for option in self.options:
+            options.add_argument(option)
+
+        self.browser = webdriver.Firefox(options=options)
+
+    def get_page_resource(self, url):
+        try:
+            self.browser.get(url)
+            page_resource = self.browser.page_source
+            return page_resource
+        except TimeoutException:
+            return None
+        finally:
+            self.browser.close()
+
+    def baidu(self, keywords):
+        tmp_results = set()
+
+        try:
+            self.browser.get('https://www.baidu.com')
+        except TimeoutException:
+            print('can not baidu')
+            return
+        key_input = self.browser.find_element_by_id('kw')
+        key_input.send_keys(keywords)
+        key_input.send_keys(Keys.ENTER)
+        time.sleep(3)
+        while 1:
+            a = self.browser.find_elements_by_class_name('c-showurl')
+            for i in a:
+                href = i.get_attribute('href')
+                if href:
+                    tmp_results.add(href)
+            try:
+                next_page = self.browser.find_element_by_link_text('下一页 >')
+                next_page.click()
+                time.sleep(3)
+            except NoSuchElementException:
+                break
+            finally:
+                self.browser.close()
+
+        results = set()
+        for result in tmp_results:
+            try:
+                resp = requests.get(result, allow_redirects=False)
+                results.add(resp.headers['Location'])
+            except Exception:
+                pass
+        return results
